309af1922e9e63e7cbe7ac4f5455103a3228f9c2
Anastasia1903 <abadlysheva@gmail.com>
2023-06-15 10:41:53 +0500

    +nastya

---
+++ projects/bal_anastasiya/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program4
+{
+    static void Main(string[] args)
+    {
+        double celsius; // градусы Цельсия
+        double fahrenheit; // градусы Фаренгейта
+        double kelvin; // Кельвины
+
+        Console.WriteLine("Введите температуру в градусах Цельсия:");
+        celsius = double.Parse(Console.ReadLine());
+
+        fahrenheit = celsius * 9.0 / 5.0 + 32.0;
+        kelvin = celsius + 273.15;
+
+        Console.WriteLine("Температура в градусах Фаренгейта: " + fahrenheit);
+        Console.WriteLine("Температура в Кельвинах: " + kelvin);
+    }
+}
